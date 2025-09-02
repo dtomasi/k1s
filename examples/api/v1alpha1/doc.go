2f25a96c062a4548ba89623cfc2ab90910e967c7
Dominik Tomasi <dominik.tomasi@gmail.com>
2025-09-02 19:07:01 +0200

    chore(examples): Add example api types
    Signed-off-by: Dominik Tomasi <dominik.tomasi@gmail.com>

--- examples/api/v1alpha1/doc.go
+++
@@ -1,4 +0,0 @@
-// Package v1alpha1 contains API definitions for the k1s inventory system,
-// demonstrating custom resource definitions (Items and Categories) with
-// kubebuilder markers for validation and CLI generation.
-package v1alpha1
